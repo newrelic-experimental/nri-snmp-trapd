f81d8f08960e93cafc7738680b62821c23b94826
Harry Kimpel <hkimpel@newrelic.com>
2021-02-12 13:56:38 +0100

    Default events type not reflected if traps event type is not set

--- src/main.go
+++ src/main.go
@@ -14,7 +14,7 @@ import (
 	//insights "github.com/newrelic/go-insights/client"
 
 	//"github.com/newrelic/newrelic-client-go/newrelic"
-	"github.com/harrykimpel/newrelic-client-go/newrelic"
+	"github.com/harrykimpel/newrelic-client-go/newrelic" // using my newrelic-client-go until PR is merged
 	log "github.com/sirupsen/logrus"
 	"github.com/soniah/gosnmp"
 	"gopkg.in/yaml.v2"
